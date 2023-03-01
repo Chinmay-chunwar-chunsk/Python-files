67f72233972b6f3f7c2b83021cff0b6ee1e20d0a
Chinmay-chunwar-chunsk <80244597+Chinmay-chunwar-chunsk@users.noreply.github.com>
2023-03-01 15:12:33 +0530

    sdfds
    fsdfsdf

--- modernUI.py
+++ modernUI.py
@@ -1,23 +1,35 @@
 import customtkinter
 
+dict={}
+
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
+def submit_button():
+    dict[username.get()]=password.get()
+    create_window.destroy()
+    print(dict)
+
 def create():
+    global create_window
     create_window=customtkinter.CTk()
     create_window.title("Create Account")
     create_window.geometry("500x350")
     create_window.minsize(500,350)
     create_window.maxsize(500,350)
 
+    frame=customtkinter.CTkFrame(master=create_window)
+    frame.pack(padx=60,pady=20,fill="both",expand=True)
 
-    label=customtkinter.CTkLabel(master=create_window,text="Account", font=("Arial", 24))
+    label=customtkinter.CTkLabel(master=frame,text="Account", font=("Arial", 24))
     label.pack(padx=20,pady=20)
-    username=customtkinter.CTkEntry(master=create_window, placeholder_text="Username",font=("Arial",18))
+    global username
+    username=customtkinter.CTkEntry(master=frame, placeholder_text="Username",font=("Arial",18))
     username.pack(padx=20,pady=20)
-    password=customtkinter.CTkEntry(master=create_window, placeholder_text="Password",show="•",font=("Aria;",18))
+    global password
+    password=customtkinter.CTkEntry(master=frame, placeholder_text="Password",show="•",font=("Aria;",18))
     password.pack(padx=20,pady=20)
-    submit=customtkinter.CTkButton(master=create_window,text="Submit",font=("Arial",18))
+    submit=customtkinter.CTkButton(master=frame,text="Submit",font=("Arial",18),command=submit_button)
     submit.pack(padx=20,pady=20)
 
     create_window.mainloop()
